127fe73429c507af13801130588e3a9e60f9f0f7
ayumi043 <ayumi043@gmail.com>
2016-12-29 10:23:59 +0800

    无
    无

--- src/BiLin3DPrint/LoggingErrorHandler.cs
+++ src/BiLin3DPrint/LoggingErrorHandler.cs
@@ -4,24 +4,33 @@ using Nancy;
 using Nancy.ErrorHandling;
 
 namespace Bilin3d {
+   
     public class LoggingErrorHandler : IStatusCodeHandler {
         private readonly ILog _logger = LogManager.GetLogger(typeof(LoggingErrorHandler));
         public void Handle(HttpStatusCode statusCode, NancyContext context) {
             object errorObject;
             context.Items.TryGetValue(NancyEngine.ERROR_EXCEPTION, out errorObject);
             var error = (errorObject as Exception).InnerException;
-                           
+
             //new Task(() => {
             //    _logger.Error("发生错误啦!", error);
             //}).Start();
 
             System.Threading.ThreadPool.QueueUserWorkItem((i) => {
-                _logger.Error("发生错误:"+error.Message +"\r\n" + error.StackTrace);
-            }); 
+                _logger.Error("发生错误:" + error.Message + "\r\n" + error.StackTrace);
+            });
         }
-        
+
         public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context) {
-            return statusCode == HttpStatusCode.InternalServerError;
+            // 开发环境下，如果试图发生错误时，不会显示错误信息，所以用域名来判断是生产环境或开发环境;
+            if (context.Request.Url.HostName == "www.3dworks.cn") {
+                //不会显示错误信息，会执行上面的Handle方法
+                return statusCode == HttpStatusCode.InternalServerError;
+            } else {
+                //会显示错误信息，不会执行上面的Handle方法
+                return false;
+            }            
         }
     }
+
 }
